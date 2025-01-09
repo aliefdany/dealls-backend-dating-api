afca3dbb416645048a830db5f084c39ce65fb4ed
aliefdany <works@alief.tech>
2025-01-10 02:19:25 +0700

    added custom validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProfilesModule } from './profiles/profiles.module';
+import { CustomValidationPipe } from './custom-validation.pipe';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [UsersModule, ProfilesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: CustomValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
