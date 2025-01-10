bee6e0e876db3a5cd1033885e2f4afb80315191a
aliefdany <works@alief.tech>
2025-01-10 18:50:48 +0700

    updated test for orders module

--- src/orders/orders.controller.spec.ts
+++ src/orders/orders.controller.spec.ts
@@ -1,20 +1,75 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { ExecutionContext } from '@nestjs/common';
+import { OrderEntity } from './entities/order.entity';
 
 describe('OrdersController', () => {
   let controller: OrdersController;
+  let service: OrdersService;
+
+  const mockOrdersService = {
+    create: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrdersController],
-      providers: [OrdersService],
-    }).compile();
+      providers: [
+        {
+          provide: OrdersService,
+          useValue: mockOrdersService,
+        },
+      ],
+    })
+      .overrideGuard(JwtAuthGuard)
+      .useValue({
+        canActivate: (context: ExecutionContext) => {
+          const request = context.switchToHttp().getRequest();
+          request.user = { id: 1 }; // Mock authenticated user
+          return true;
+        },
+      })
+      .compile();
 
     controller = module.get<OrdersController>(OrdersController);
+    service = module.get<OrdersService>(OrdersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should call OrdersService.create with correct parameters and return an OrderEntity', async () => {
+      const createOrderDto: CreateOrderDto = {
+        packageId: 12,
+      };
+      const userId = 1;
+      const mockOrderEntity: OrderEntity = {
+        id: 2,
+        order_id: 12,
+        status: 'pending',
+        order: {
+          id: 12,
+          user_id: userId,
+          package_id: createOrderDto.packageId,
+        },
+      };
+
+      mockOrdersService.create.mockResolvedValue(mockOrderEntity);
+
+      const response = await controller.create(createOrderDto, {
+        user: { id: userId },
+      });
+
+      expect(mockOrdersService.create).toHaveBeenCalledWith(
+        createOrderDto,
+        userId,
+      );
+      expect(response).toEqual(mockOrderEntity);
+    });
   });
 });
--- src/orders/orders.service.spec.ts
+++ src/orders/orders.service.spec.ts
@@ -1,18 +1,110 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersService } from './orders.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('OrdersService', () => {
   let service: OrdersService;
+  let prismaService: PrismaService;
+
+  const mockPrismaService = {
+    order: {
+      create: jest.fn(),
+    },
+    payment: {
+      create: jest.fn(),
+    },
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrdersService],
+      providers: [
+        OrdersService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+      ],
     }).compile();
 
     service = module.get<OrdersService>(OrdersService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('createOrder', () => {
+    it('should create a new order and return it', async () => {
+      const packageId = 12;
+      const userId = 1;
+      const mockOrder = { id: 101, user_id: userId, package_id: packageId };
+
+      mockPrismaService.order.create.mockResolvedValue(mockOrder);
+
+      const result = await service.createOrder(packageId, userId);
+
+      expect(prismaService.order.create).toHaveBeenCalledWith({
+        data: { user_id: userId, package_id: packageId },
+      });
+      expect(result).toEqual(mockOrder);
+    });
+  });
+
+  describe('createPayment', () => {
+    it('should create a new payment and return it', async () => {
+      const orderId = 101;
+      const mockPayment = {
+        id: 201,
+        order_id: orderId,
+        status: 'PENDING',
+        order: { id: orderId, items: [], total: 100 },
+      };
+
+      mockPrismaService.payment.create.mockResolvedValue(mockPayment);
+
+      const result = await service.createPayment(orderId);
+
+      expect(prismaService.payment.create).toHaveBeenCalledWith({
+        data: {
+          order_id: orderId,
+          status: 'PENDING',
+        },
+        include: {
+          order: true,
+        },
+      });
+      expect(result).toEqual(mockPayment);
+    });
+  });
+
+  describe('create', () => {
+    it('should create an order and payment and return the payment', async () => {
+      const createOrderDto = { packageId: 12 };
+      const userId = 1;
+      const mockOrder = {
+        id: 101,
+        user_id: userId,
+        package_id: createOrderDto.packageId,
+      };
+      const mockPayment = {
+        id: 201,
+        order_id: mockOrder.id,
+        status: 'PENDING',
+        order: mockOrder,
+      };
+
+      jest.spyOn(service, 'createOrder').mockResolvedValue(mockOrder);
+      jest.spyOn(service, 'createPayment').mockResolvedValue(mockPayment);
+
+      const result = await service.create(createOrderDto, userId);
+
+      expect(service.createOrder).toHaveBeenCalledWith(
+        createOrderDto.packageId,
+        userId,
+      );
+      expect(service.createPayment).toHaveBeenCalledWith(mockOrder.id);
+      expect(result).toEqual(mockPayment);
+    });
   });
 });
