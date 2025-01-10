3f34464c190a4bd9405bce79269193e1ef48a022
aliefdany <works@alief.tech>
2025-01-10 19:10:27 +0700

    updated test at profile module

--- src/profiles/profiles.controller.spec.ts
+++ src/profiles/profiles.controller.spec.ts
@@ -1,20 +1,63 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProfilesController } from './profiles.controller';
 import { ProfilesService } from './profiles.service';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { ExecutionContext } from '@nestjs/common';
 
 describe('ProfilesController', () => {
   let controller: ProfilesController;
+  let profilesService: ProfilesService;
+
+  const mockProfilesService = {
+    getProfile: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProfilesController],
-      providers: [ProfilesService],
-    }).compile();
+      providers: [
+        {
+          provide: ProfilesService,
+          useValue: mockProfilesService,
+        },
+      ],
+    })
+      .overrideGuard(JwtAuthGuard)
+      .useValue({
+        canActivate: (context: ExecutionContext) => {
+          const request = context.switchToHttp().getRequest();
+          request.user = { id: 1 }; // Mock authenticated user
+          return true;
+        },
+      })
+      .compile();
 
     controller = module.get<ProfilesController>(ProfilesController);
+    profilesService = module.get<ProfilesService>(ProfilesService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getProfile', () => {
+    it('should fetch a profile for the authenticated user', async () => {
+      const userId = 1;
+      const tz = 'Asia/Jakarta';
+      const mockProfile = {
+        id: 2,
+        swipee_id: 3,
+        swiper_id: userId,
+        date: new Date(),
+        like: false,
+      };
+
+      mockProfilesService.getProfile.mockResolvedValue(mockProfile);
+
+      const result = await controller.getProfile({ user: { id: userId } }, tz);
+
+      expect(profilesService.getProfile).toHaveBeenCalledWith(tz, userId);
+      expect(result).toEqual(mockProfile);
+    });
   });
 });
--- src/profiles/profiles.service.spec.ts
+++ src/profiles/profiles.service.spec.ts
@@ -1,18 +1,148 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProfilesService } from './profiles.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { BadRequestException } from '@nestjs/common';
+import { ProfileContext } from './package-strategy/package-strategy.context';
+import { RegularPackageStrategy } from './package-strategy/concrete-strategies/package-strategy.concrete.regular';
+import { PremiumPackageStrategy } from './package-strategy/concrete-strategies/package-strategy.concrete.premium';
+
+// Mock ProfileContext class
+jest.mock('./package-strategy/package-strategy.context');
 
 describe('ProfilesService', () => {
   let service: ProfilesService;
+  let prismaService: PrismaService;
+
+  const mockPrismaService = {
+    profile: {
+      findUnique: jest.fn(),
+      update: jest.fn(),
+    },
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProfilesService],
+      providers: [
+        ProfilesService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+      ],
     }).compile();
 
     service = module.get<ProfilesService>(ProfilesService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getProfile', () => {
+    it('should return profile for regular package', async () => {
+      const userId = 1;
+      const tz = 'Asia/Jakarta';
+      const mockProfile = {
+        id: userId,
+        package: { name: 'Regular' },
+      };
+
+      mockPrismaService.profile.findUnique.mockResolvedValue(mockProfile);
+
+      // Mock return value for viewProfile method
+      const mockProfileData = {
+        id: 1,
+        swiper_id: 1,
+        swipee_id: 2,
+        date: new Date(),
+        like: true,
+      };
+
+      // Mock the viewProfile method of ProfileContext to return the mock profile data
+      ProfileContext.prototype.viewProfile = jest
+        .fn()
+        .mockResolvedValue(mockProfileData);
+
+      const result = await service.getProfile(tz, userId);
+
+      expect(mockPrismaService.profile.findUnique).toHaveBeenCalledWith({
+        where: { user_id: userId },
+        include: { package: true },
+      });
+      expect(ProfileContext).toHaveBeenCalledWith(
+        expect.any(RegularPackageStrategy),
+      ); // Assert ProfileContext constructor was called
+      expect(result).toBe(mockProfileData); // Check if it matches the mock profile data
+    });
+
+    it('should return profile for premium package', async () => {
+      const userId = 1;
+      const tz = 'Asia/Jakarta';
+      const mockProfile = {
+        id: userId,
+        package: { name: 'Premium' },
+      };
+
+      mockPrismaService.profile.findUnique.mockResolvedValue(mockProfile);
+
+      // Mock return value for viewProfile method
+      const mockProfileData = {
+        id: 1,
+        swiper_id: 1,
+        swipee_id: 2,
+        date: new Date(),
+        like: true,
+      };
+
+      // Mock the viewProfile method of ProfileContext to return the mock profile data
+      ProfileContext.prototype.viewProfile = jest
+        .fn()
+        .mockResolvedValue(mockProfileData);
+
+      const result = await service.getProfile(tz, userId);
+
+      expect(mockPrismaService.profile.findUnique).toHaveBeenCalledWith({
+        where: { user_id: userId },
+        include: { package: true },
+      });
+      expect(ProfileContext).toHaveBeenCalledWith(
+        expect.any(PremiumPackageStrategy),
+      ); // Assert ProfileContext constructor was called
+      expect(result).toBe(mockProfileData); // Check if it matches the mock profile data
+    });
+
+    it('should throw BadRequestException if package is invalid', async () => {
+      const userId = 1;
+      const tz = 'Asia/Jakarta';
+      const mockProfile = {
+        id: userId,
+        package: { name: 'InvalidPackage' },
+      };
+
+      mockPrismaService.profile.findUnique.mockResolvedValue(mockProfile);
+
+      await expect(service.getProfile(tz, userId)).rejects.toThrow(
+        BadRequestException,
+      );
+    });
+  });
+
+  describe('updatePackage', () => {
+    it('should update the user profile package', async () => {
+      const userId = 1;
+      const packageId = 2;
+      const mockUpdatedProfile = { id: userId, package_id: packageId };
+
+      mockPrismaService.profile.update.mockResolvedValue(mockUpdatedProfile);
+
+      const result = await service.updatePackage(userId, packageId);
+
+      expect(mockPrismaService.profile.update).toHaveBeenCalledWith({
+        where: { user_id: userId },
+        data: { package_id: packageId },
+      });
+      expect(result).toEqual(mockUpdatedProfile);
+    });
   });
 });
